c612f154bd8a5f5250e1097aa392f2017be4cf52
Sjoerd van der Kraan <16091183@student.hhs.nl>
2018-09-29 12:20:47 +0200

    Explore cars finish cards and nav

--- frontend/src/interfaces/CarInterface.ts
+++ frontend/src/interfaces/CarInterface.ts
@@ -2,6 +2,8 @@ export default interface CarInterface {
     id: number;
     brand: string;
     type: string;
+    description: string;
     price: number;
+    image: string;
     color: string[];
 }
--- frontend/src/views/ExploreCars.vue
+++ frontend/src/views/ExploreCars.vue
@@ -1,10 +1,22 @@
 <template>
-    <div>
+    <div class="explore-cars">
         <nav class="main-nav">
             <div class="container">
                 <div class="row">
                     <div class="col logo logo--white logo--smaller">
                         <main-logo />
+                    </div>
+
+                    <div class="header-nav__menu">
+                        <ul class="list-unstyled">
+                            <li>
+                                <router-link :to="{name: 'home'}">Home</router-link>
+                            </li>
+                            <li class="active">
+                                <router-link :to="{name: 'explore-cars'}">Explore Cars</router-link>
+                            </li>
+                            <li><a href="#"><i class="far fa-user"></i></a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -17,26 +29,36 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-4" v-for="car in cars" :key="car.id">
+                <div class="col-md-4 card-padding" v-for="car in cars" :key="car.id">
                     <div class="box car-card">
                         <div class="car-card__header">
                             <figure>
-                                <img src="https://via.placeholder.com/400x400/" class="img-fluid" alt="">
+                                <img :src="`img/${car.image}`" class="img-fluid" alt="">
                             </figure>
                         </div>
                         <div class="car-card__body">
                             <div class="car-card__body-top">
-                                <span class="car-price">€100,-</span>
+                                
+                                <span class="car-price">€{{ car.price }},-</span>
                                 <ul class="car-colors">
                                     <li v-for="color in car.colors" :key="color" class="car-colors__item">
                                         <span :style="{ background: color }"></span>
                                     </li>
                                 </ul>
                             </div>
-                        </div>
 
-                        {{ car.name }}
-                        <!--{{ car.description }}-->
+                            <div class="car-card__body-middle">
+                                <span class="car-brand">{{ car.brand }} {{ car.type }}</span>
+                                <span class="car-description">{{ car.description }}</span>
+                            </div>
+
+                            <div class="car-card__body-footer">
+                                <button class="btn btn-primary">
+                                    <span>Customize</span>
+                                    <i class="fas fa-wrench"></i>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
