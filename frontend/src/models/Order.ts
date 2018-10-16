e842920ccdb4c96fb76ebab0e049c1cbe0059b15
Danny Nieuwmans <dannynieuwmans@gmail.com>
2018-10-16 20:36:51 +0200

    Start with the order panels

--- frontend/src/models/Order.ts
+++ frontend/src/models/Order.ts
@@ -4,6 +4,7 @@ import OrderInterface from '@/interfaces/OrderInterface';
 export enum stepsEnum {
     COLOR = 1,
     ACCESSORY,
+    USER_INFO,
     SUMMARY,
 }
 
@@ -16,6 +17,11 @@ const stepsDef = [
     {
         id: stepsEnum.ACCESSORY,
         name: 'Accessory',
+        completed: 0,
+    },
+    {
+        id: stepsEnum.USER_INFO,
+        name: 'User Info',
         completed: 0,
     },
     {
@@ -35,6 +41,23 @@ export class Order implements OrderInterface{
     public selectedCar: Car;
     public selectedColor: string;
     public activeStep: number;
+
+    get activeStepName() {
+        return this.steps[this.activeStep - 1].name;
+    }
+
+    get totalPrice() {
+        // TODO: implement correctly
+        let price = this.selectedCar.price;
+
+        return `€${price.toLocaleString('nl-NL')},-`;
+    }
+
+    get selectedColorObject() {
+        const name = this.selectedCar.colors[this.selectedColor as any].name;
+
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
 
     constructor(params: OrderInterface) {
         this.id = params.id;
@@ -63,6 +86,16 @@ export class Order implements OrderInterface{
 
         if (this.activeStep !== stepsEnum.SUMMARY) {
             this.activeStep += 1;
+        }
+
+        this.store();
+    }
+
+    public previousStep() {
+        this.steps[this.activeStep - 2].completed = 0;
+
+        if (this.activeStep !== stepsEnum.COLOR) {
+            this.activeStep -= 1;
         }
 
         this.store();
