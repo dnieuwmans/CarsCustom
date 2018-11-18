c567748ab725ed791a789f4f5d50fa7fa2730bf0
Danny Nieuwmans <dannynieuwmans@gmail.com>
2018-11-18 14:07:26 +0100

    Update alert in explore cars

--- frontend/src/views/ExploreCars/ExploreCars.vue
+++ frontend/src/views/ExploreCars/ExploreCars.vue
@@ -7,7 +7,9 @@
                 <div class="col">
                     <h3>Explore Cars</h3>
 
-                    <div class="alert alert-danger mb-4" v-if="showErrorMessage">Oops, you didn't select a car yet!
+                    <div class="alert alert-danger mb-4" v-if="showErrorMessage">
+                        <i class="far fa-times"></i>
+                        Oops, you didn't select a car yet!
                     </div>
                 </div>
             </div>
